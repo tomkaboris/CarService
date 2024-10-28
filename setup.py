dd9714adb6887b677a9dcb0ce48ddd738e3d6f5e
tomkaboris <tomkaboris@gmail.com>
2024-10-28 20:41:52 +0100

    Add new file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    'packages': [],
+    'includes': [],
+    'include_files': ['app_data.db','tb.ico','README.md'],
+}
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'  # Suppress console window
+
+setup(
+    name='dpfpa',
+    version='1.0',
+    description='Boris Tomka Application',
+    options={'build_exe': build_exe_options},
+    executables=[Executable(
+        'dpfpa.py',
+        base=base,
+        icon='tb.ico',    # This is where you specify the icon
+        target_name='dpfpa.exe'  # Optional: specify the name of the executable
+    )]
+)
+
+# python setup.py build
